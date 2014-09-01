cda56a655d7fecfbf05f92555c1d2328e1259b4c
Tim Higgison <tim.higgison@gmail.com>
2014-09-01 23:31:13 +1000

    Updated with CSV example and class for static typing

--- realworld.py
+++ realworld.py
@@ -1,6 +1,13 @@
 import csv
 
-with open('/home/tim/Desktop/CatHierarchy.csv') as csvfile:
-    csvfilereader = csv.reader(csvfile)
-    for row in csvfilereader:
-        print row
+
+# class CatHierarchyRecord:
+#     def __init__(self, row):
+#         self.cat_hierarchy_code = row[0]
+#         self.cat_hierarchy_type = row[2]
+
+
+with open('/home/tim/Desktop/CatHierarchy.ascii.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
+    catalogues = [line[0] for line in csvreader if len(line) >= 3 and line[2] == "1"]
+    print(catalogues)
